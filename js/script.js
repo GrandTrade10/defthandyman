44bc3d7ad4e9ae3cad349ec1f117a84eaa15b775
Ley peng <82555921+GrandTrade10@users.noreply.github.com>
2022-12-19 14:43:14 +0800

    Replace script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+window.onload = function() {
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+}
+
 /* slide show */
 var slideIndex = [1,1];
 var slideId = ["mySlides1", "mySlides2"]
@@ -20,3 +43,18 @@ function showSlides(n, no) {
 }
 
 /* end slide show*/
+
+
+/* Hiding panel */
+            function myFunction() {
+                var x = document.getElementById("myDIV");
+                if (x.style.display === "none") {
+                    x.style.display = "block";
+                } else {
+                    x.style.display = "none";
+                }
+
+                
+            }
+/* End Hiding Panel */
+
